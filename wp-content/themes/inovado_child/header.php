c797b77a75dbfac38e272066a05164808ab46e14
Shawn Li <tp9106@gmail.com>
2016-10-13 10:42:42 +0800

    clean up header code

--- wp-content/themes/inovado_child/header.php
+++ wp-content/themes/inovado_child/header.php
@@ -48,32 +48,36 @@
 
 	<div class="wrapall">
 	
-	<?php if($data['check_topbar'] == true) { ?>
-	<div id="topbar" class="clearfix <?php if($data['check_socialtopbar'] == false) { echo 'no-social'; } ?>">
-	
-		<div class="container">
-		
-			<div class="eight columns">
-				<?php if($data['text_callus'] != "") { ?>
-					<div class="callus"><?php echo $data['text_callus']; ?></div>
-					<div class="clear"></div>
-				<?php } ?>
-			</div>
-			
-			<?php if($data['check_socialtopbar'] == true) { ?>
-			<div class="eight columns">
-				<div class="social-icons clearfix">
-					<ul>
-					</ul>
-				</div>
-			</div>
-			<?php } ?>
-		
-		</div>
-	
-	</div> <!-- end topbar -->
-	<?php } ?>
-
-	<?php
-            include_once('framework/inc/headers/header.php');
-	?>		
+                <header id="header" class="header clearfix">
+                
+                        <div class="container header-container">
+                
+                                <div class="sixteen columns clearfix">
+                
+                                        <div class="six columns alpha">
+                
+                                                <div class="logo">
+                
+                                                        <a href="<?php echo home_url(); ?>/"><img src="wp-content/uploads/2015/10/focusconlaw_logo.png" alt="<?php bloginfo('name'); ?>" class="logo_standard" /></a>
+                                                                <?php if($data['media_logo_retina'] != '') { ?><a href="<?php echo home_url(); ?>/"><img src="<?php echo $data['media_logo_retina'] ?>" width="<?php echo $data['logo_width']; ?>" height="<?php echo $data['logo_height']; ?>" alt="<?php bloginfo('name'); ?>" class="logo_retina" /></a><?php } ?>
+                                                </div>
+                                        </div>
+               
+                                        <form action="<?php echo home_url(); ?>/" id="header-searchform" method="get">
+                                                <input type="text" id="header-s" name="s" value="" autocomplete="off" />
+                                                <input type="submit" value="Search" id="header-searchsubmit" />
+                                        </form>
+                
+                                </div>
+                
+                        </div>
+                
+                        <div id="navigation" class="sixteen columns clearfix alpha omega">
+                                <div class="container">
+                                        <div class="sixteen columns">
+                                                <?php wp_nav_menu(array('theme_location' => 'main_navigation', 'menu_id' => 'nav')); ?>
+                                        </div>
+                                </div>
+                        </div>
+                
+                </header>
